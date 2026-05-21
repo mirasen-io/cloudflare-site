98f2ff2b7fb8dc3491a01e4285ea4bfee38aa657
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2026-05-21 19:01:08 +0200

    scripts

--- scripts/package-review-src.sh
+++ scripts/package-review-src.sh
@@ -57,7 +57,7 @@ if [[ ! -d "$ROOT_DIR/deploy-site" ]]; then
 fi
 
 # Standard payload
-cp -R "$ROOT_DIR/deploy-site" "$STAGING_DIR/src"
+cp -R "$ROOT_DIR/deploy-site" "$STAGING_DIR/deploy-site"
 copy_if_exists "tests"
 copy_if_exists "scripts"
 copy_if_exists "docs"
